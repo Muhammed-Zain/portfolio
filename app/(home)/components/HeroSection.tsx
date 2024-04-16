d9535776505b08eb9b3be213d295b4224f67a2f3
Muhammed-Zain <mohdzain2002@gmail.com>
2024-04-16 23:41:54 +0530

    add text generate effect to hero section

--- app/(home)/components/HeroSection.tsx
+++ app/(home)/components/HeroSection.tsx
@@ -2,17 +2,21 @@ import { MovingBorderBtn } from '@/components/ui/moving-border'
 import Link from 'next/link'
 import React from 'react'
 import Title from './Title'
+import { TextGenerateEffect } from '@/components/ui/text-generate-effext'
 
 const HeroSection = () => {
+
+     const textGenerateWord = " Based in Bangalore, I am a Fullstack developer passionate about building modern web applications that clients love."
+
      return (
           <div className='min-h-[50vh] md:min-h-[60vh] gap-14 flex flex-col-reverse items-center justify-between lg:flex-row lg:gap-0 animate-move-up' >
                <div className='space-y-10 text-center lg:text-left'>
-                    <h1 className='text-4xl lg:text-7xl font-bold mt-10 md:mt-0'>
+                    <div className='text-4xl lg:text-7xl font-bold mt-10 md:mt-0'>
                          Nice to meet you! <span className='hidden md:inline-block'>👋</span><br /> <span className='underline underline-offset-8 decoration-[#914d76]'>{"I'm Muhammed Zain."}</span>
-                    </h1>
-                    <p className='md:w-96 text-lg text-gray-300'>
-                         {"Based in Bangalore, I'm a Fullstack developer passionate about building modern web applications that clients love."}
-                    </p>
+                    </div>
+                    <div className='md:w-[75%] font-bold md:text-3xl text-lg text-gray-400'>
+                         <TextGenerateEffect words={textGenerateWord} />
+                    </div>
                     <div className='md:flex gap-5'>
                          <Link href={"mailto:mohdzain2002@gmail.com"} target="_blank" className='inline-block group mb-12 md:mb-0'>
                               <Title text='Contact Me! 📫' className='text-3xl' />
