5dda90f1d23c16d929197b837c64ca33fce09f09
Muhammed-Zain <mohdzain2002@gmail.com>
2024-04-17 13:23:01 +0530

    fix futher button change

--- app/(home)/components/HeroSection.tsx
+++ app/(home)/components/HeroSection.tsx
@@ -35,7 +35,7 @@ const HeroSection = () => {
 
                                              <g id="SVGRepo_iconCarrier"> <path d="M3 15C3 17.8284 3 19.2426 3.87868 20.1213C4.75736 21 6.17157 21 9 21H15C17.8284 21 19.2426 21 20.1213 20.1213C21 19.2426 21 17.8284 21 15" stroke="#f3ffbd" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" /> <path d="M12 3V16M12 16L16 11.625M12 16L8 11.625" stroke="#f3ffbd" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" /> </g>
 
-                                        </svg> <div>Download Resume</div></h1>
+                                        </svg> <div className='text-sm md:text-xl'>Download Resume</div></h1>
                               </Link>
                          </MovingBorderBtn>
                     </div>
