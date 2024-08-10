bd864d7e35cad448c1eea054054cd8fc295c56d1
Muhammed-Zain <mohdzain2002@gmail.com>
2024-08-11 00:36:27 +0530

    change email, metadata image, and resume

--- app/(home)/components/HeroSection.tsx
+++ app/(home)/components/HeroSection.tsx
@@ -18,22 +18,22 @@ const HeroSection = () => {
                          <TextGenerateEffect words={textGenerateWord} />
                     </div>
                     <div className='flex gap-5 flex-col md:flex-row justify-center lg:justify-start items-center md:items-start'>
-                         <Link href={"mailto:mohdzain2002@gmail.com"} target="_blank" className='inline-block group mb-12 md:mb-0'>
+                         <Link href={"mailto:muhammedzainjambagi@gmail.com"} target="_blank" className='inline-block group mb-12 md:mb-0'>
                               <Title text='Contact Me! 📫' className='text-3xl flex flex-col sm:items-center md:items-start' />
                          </Link>
                          <MovingBorderBtn
                               borderRadius="1rem"
                               className="p-3 font-semibold group-hover:bg-[#0f172a]/15 transition-all duration-500 ease-in-out group-hover:backdrop-blur-lg"
                          >
-                              <Link href={"https://drive.google.com/file/d/1yEiN4QOuIaMVxNkhKO_7GksyzBghr8sh/view"} target="_blank">
+                              <Link href={"https://drive.google.com/file/d/1AurjdropElA26EG1ImTI0noc5WAT8lhp/view?usp=sharing"} target="_blank">
                                    <h1 className='text-xl flex justify-center items-center gap-5 '>
                                         <svg width="10%" height="10%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 
-                                             <g id="SVGRepo_bgCarrier" stroke-width="0" />
+                                             <g id="SVGRepo_bgCarrier" strokeWidth="0" />
 
-                                             <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" />
+                                             <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" />
 
-                                             <g id="SVGRepo_iconCarrier"> <path d="M3 15C3 17.8284 3 19.2426 3.87868 20.1213C4.75736 21 6.17157 21 9 21H15C17.8284 21 19.2426 21 20.1213 20.1213C21 19.2426 21 17.8284 21 15" stroke="#f3ffbd" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" /> <path d="M12 3V16M12 16L16 11.625M12 16L8 11.625" stroke="#f3ffbd" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" /> </g>
+                                             <g id="SVGRepo_iconCarrier"> <path d="M3 15C3 17.8284 3 19.2426 3.87868 20.1213C4.75736 21 6.17157 21 9 21H15C17.8284 21 19.2426 21 20.1213 20.1213C21 19.2426 21 17.8284 21 15" stroke="#f3ffbd" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" /> <path d="M12 3V16M12 16L16 11.625M12 16L8 11.625" stroke="#f3ffbd" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" /> </g>
 
                                         </svg> <div className='text-sm md:text-xl'>Download Resume</div></h1>
                               </Link>
